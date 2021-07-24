39bfcf516b489d70a0641cf9f536479196baf21c
ShashankTapre <82704711+ShashankTapre@users.noreply.github.com>
2021-07-24 17:43:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -89,12 +89,12 @@ function keyPressed () {
 		translation={x:-20,y:0}
 		Matter.Body.translate(packageBody,translation)
 	
-	}else if (keyCode=RIGHT_ARROW) {
+	}else if (keyCode===RIGHT_ARROW) {
 		helicopterSprite.x=helicopterSprite.x+20;
 		translation={x:20,y:0}
 	Matter.Body.translate(packageBody,translation)
 	}
-	else if (keyCode==DOWN_ARROW) {
+	else if (keyCode===DOWN_ARROW) {
 		Matter.Body.setStatic(packageBody,false);
 	}
 
